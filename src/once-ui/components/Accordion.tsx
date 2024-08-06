2e72a8e9094ff416ad152421f3aa5fd6aabdd9e6
Lorant <lorant.toth.94@gmail.com>
2024-08-06 15:38:06 +0200

    refactor: improve Accordion functionality

--- src/once-ui/components/Accordion.tsx
+++ src/once-ui/components/Accordion.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
-
+import React, { useState, useEffect, forwardRef, useImperativeHandle, useRef } from 'react';
 import { Flex, Icon, Heading } from '.';
 
 interface AccordionProps {
-    title: string;
+    title: React.ReactNode;
     children: React.ReactNode;
     style?: React.CSSProperties;
     className?: string;
@@ -20,20 +19,58 @@ const Accordion: React.FC<AccordionProps> = forwardRef(({
     open = false
 }, ref) => {
     const [isOpen, setIsOpen] = useState(open);
+    const [maxHeight, setMaxHeight] = useState(open ? 'none' : '0px');
+    const contentRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        setIsOpen(open);
+        if (open) {
+            setIsOpen(true);
+            setMaxHeight(`${contentRef.current?.scrollHeight}px`);
+        } else {
+            setIsOpen(false);
+            setMaxHeight('0px');
+        }
     }, [open]);
 
     const toggleAccordion = () => {
+        if (isOpen) {
+            setMaxHeight('0px');
+        } else {
+            setMaxHeight(`${contentRef.current?.scrollHeight}px`);
+        }
         setIsOpen(!isOpen);
     };
 
     useImperativeHandle(ref, () => ({
         toggle: toggleAccordion,
-        open: () => setIsOpen(true),
-        close: () => setIsOpen(false)
+        open: () => {
+            setIsOpen(true);
+            setMaxHeight(`${contentRef.current?.scrollHeight}px`);
+        },
+        close: () => {
+            setIsOpen(false);
+            setMaxHeight('0px');
+        }
     }));
+
+    useEffect(() => {
+        const handleTransitionEnd = () => {
+            if (isOpen) {
+                setMaxHeight('none');
+            }
+        };
+
+        const contentElement = contentRef.current;
+        if (contentElement) {
+            contentElement.addEventListener('transitionend', handleTransitionEnd);
+        }
+
+        return () => {
+            if (contentElement) {
+                contentElement.removeEventListener('transitionend', handleTransitionEnd);
+            }
+        };
+    }, [isOpen]);
 
     return (
         <Flex
@@ -44,32 +81,41 @@ const Accordion: React.FC<AccordionProps> = forwardRef(({
             <Flex 
                 style={{ borderTop: "1px solid var(--neutral-border-medium)", cursor: 'pointer' }}
                 paddingY="16"
-                paddingLeft="12"
-                paddingRight="24"
+                paddingLeft="m"
+                paddingRight="m"
                 alignItems="center"
                 justifyContent="space-between"
+                tabIndex={0}
                 onClick={toggleAccordion}
                 aria-expanded={isOpen}
                 aria-controls="accordion-content">
                 <Heading
                     as="h3"
-                    variant="heading-strong-l">
+                    variant="heading-strong-s">
                     {title}
                 </Heading>
                 <Icon
                     name="chevronDown"
                     size="m"
-                    style={{ transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)', transition: 'transform 0.3s' }} />
+                    style={{ display: 'flex', transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)', transition: 'transform var(--transition-duration-micro-medium)' }} />
             </Flex>
-            {isOpen && (
+            <div
+                id="accordion-content"
+                ref={contentRef}
+                style={{
+                    maxHeight,
+                    overflow: 'hidden',
+                    transition: 'max-height var(--transition-duration-macro-long) var(--transition-timing-function)',
+                    visibility: isOpen ? 'visible' : 'hidden'
+                }}
+                aria-hidden={!isOpen}>
                 <Flex
-                    id="accordion-content"
-                    paddingX="12"
+                    paddingX="m"
                     paddingBottom="32"
                     direction="column">
                     {children}
                 </Flex>
-            )}
+            </div>
         </Flex>
     );
 });
