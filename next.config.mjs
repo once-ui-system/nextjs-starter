50a76360945eada34a25c83d0c0a1b337c33bdcc
lightyfr <127269031+lightyfr@users.noreply.github.com>
2024-11-11 16:05:04 -0500

    feat: update Next.js configuration and migrate to Sass module system

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import * as sass from "sass";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
 
 export default nextConfig;
