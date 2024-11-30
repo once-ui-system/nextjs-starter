8abe3f7ef00e9b9b27fbe1885dc74c10b6060bee
Lorant <lorant@once-ui.com>
2024-11-30 10:43:40 +0100

    simplify syntax with classNames, make props optional

--- src/once-ui/components/NavIcon.tsx
+++ src/once-ui/components/NavIcon.tsx
@@ -1,15 +1,21 @@
 import React, { forwardRef } from 'react';
 import styles from './NavIcon.module.scss';
 import { Flex } from '.';
+import classNames from 'classnames';
 
 interface NavIconProps {
-    className: string;
-    style: React.CSSProperties;
-    onClick: () => void;
+    className?: string;
+    style?: React.CSSProperties;
+    onClick?: () => void;
     isActive: boolean;
 }
 
-const NavIcon = forwardRef<HTMLDivElement, Partial<NavIconProps>>(({ className, isActive, style, onClick }, ref) => {
+const NavIcon = forwardRef<HTMLDivElement, Partial<NavIconProps>>(({
+    className,
+    isActive,
+    style,
+    onClick
+}, ref) => {
 
     return (
         <Flex
@@ -17,11 +23,11 @@ const NavIcon = forwardRef<HTMLDivElement, Partial<NavIconProps>>(({ className, 
             tabIndex={0}
             radius="m"
             position="relative"
-            className={`${styles.button} ${className || ''}`}
+            className={classNames(styles.button, className || '')}
             style={{ ...style }}
             onClick={onClick}>
-            <div className={`${styles.line} ${isActive ? `${styles.active}` : ''}`} />
-            <div className={`${styles.line} ${isActive ? `${styles.active}` : ''}`} />
+            <div className={classNames(styles.line, isActive && styles.active)} />
+            <div className={classNames(styles.line, isActive && styles.active)} />
         </Flex>
     );
 });
