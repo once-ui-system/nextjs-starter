cd9b45ef83762aa514c7e9cf1c57e7862aeedc29
Lorant <lorant@once-ui.com>
2024-12-11 20:14:34 +0100

    create ElementType utility to render element type based on href

---
+++ src/once-ui/components/ElementType.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode, forwardRef } from "react";
+import Link from "next/link";
+
+interface ElementTypeProps {
+  href?: string;
+  children: ReactNode;
+  [key: string]: any;
+}
+
+const isExternalLink = (url: string) => /^https?:\/\//.test(url);
+
+const ElementType = forwardRef<HTMLElement, ElementTypeProps>(({ href, children, ...props }, ref) => {
+  if (href) {
+    const isExternal = isExternalLink(href);
+    if (isExternal) {
+      return (
+        <a href={href} target="_blank" rel="noreferrer" ref={ref as React.Ref<HTMLAnchorElement>} {...props as React.AnchorHTMLAttributes<HTMLAnchorElement>}>
+          {children}
+        </a>
+      );
+    }
+    return (
+      <Link href={href} ref={ref as React.Ref<HTMLAnchorElement>} {...props as React.AnchorHTMLAttributes<HTMLAnchorElement>}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button ref={ref as React.Ref<HTMLButtonElement>} {...props as React.ButtonHTMLAttributes<HTMLButtonElement>}>
+      {children}
+    </button>
+  );
+});
+
+ElementType.displayName = 'ElementType';
+
+export { ElementType };
