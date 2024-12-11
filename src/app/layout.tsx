837af29b943bc42a51726ea357145c1557844812
Lorant <lorant@once-ui.com>
2024-12-11 19:30:17 +0100

    update formatting of layout + page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,38 @@
 import "@/once-ui/styles/index.scss";
 import "@/once-ui/tokens/index.scss";
 
-import classNames from 'classnames';
+import classNames from "classnames";
 import { headers } from "next/headers";
 import { Metadata } from "next";
 
-import { baseURL, style, meta, og, schema, social } from "@/once-ui/resources/config"
+import {
+  baseURL,
+  style,
+  meta,
+  og,
+  schema,
+  social,
+} from "@/once-ui/resources/config";
 
-import { Background, Flex } from '@/once-ui/components'
+import { Background, Flex } from "@/once-ui/components";
 
-import { Inter } from 'next/font/google'
-import { Roboto_Mono } from 'next/font/google';
+import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 
 const primary = Inter({
-	variable: '--font-primary',
-	subsets: ['latin'],
-	display: 'swap',
-})
+  variable: "--font-primary",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const code = Roboto_Mono({
-	variable: '--font-code',
-	subsets: ['latin'],
-	display: 'swap',
+  variable: "--font-code",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 type FontConfig = {
-    variable: string;
+  variable: string;
 };
 
 /*
@@ -35,94 +42,101 @@ type FontConfig = {
 const secondary: FontConfig | undefined = undefined;
 const tertiary: FontConfig | undefined = undefined;
 /*
-*/
+ */
 
 export async function generateMetadata(): Promise<Metadata> {
-	const host = (await headers()).get("host");
-	const metadataBase = host ? new URL(`https://${host}`) : undefined;
+  const host = (await headers()).get("host");
+  const metadataBase = host ? new URL(`https://${host}`) : undefined;
 
-	return {
-		title: meta.title,
-		description: meta.description,
-		openGraph: {
-			title: og.title,
-			description: og.description,
-			url: 'https://' + baseURL,
-			type: og.type as
-				| "website"
-				| "article"
-				| "book"
-				| "profile"
-				| "music.song"
-				| "music.album"
-				| "music.playlist"
-				| "music.radio_station"
-				| "video.movie"
-				| "video.episode"
-				| "video.tv_show"
-				| "video.other",
-		},
-		metadataBase,
-	};
+  return {
+    title: meta.title,
+    description: meta.description,
+    openGraph: {
+      title: og.title,
+      description: og.description,
+      url: "https://" + baseURL,
+      type: og.type as
+        | "website"
+        | "article"
+        | "book"
+        | "profile"
+        | "music.song"
+        | "music.album"
+        | "music.playlist"
+        | "music.radio_station"
+        | "video.movie"
+        | "video.episode"
+        | "video.tv_show"
+        | "video.other",
+    },
+    metadataBase,
+  };
 }
 
 const schemaData = {
-	"@context": "https://schema.org",
-	"@type": schema.type,
-	"url": "https://" + baseURL,
-	"logo": schema.logo,
-	"name": schema.name,
-	"description": schema.description,
-	"email": schema.email,
-	"sameAs": Object.values(social).filter(Boolean)
+  "@context": "https://schema.org",
+  "@type": schema.type,
+  url: "https://" + baseURL,
+  logo: schema.logo,
+  name: schema.name,
+  description: schema.description,
+  email: schema.email,
+  sameAs: Object.values(social).filter(Boolean),
 };
 
 export default function RootLayout({
-  	children,
+  children,
 }: Readonly<{
-  	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<Flex
-			as="html" lang="en"
-			fillHeight background="page"
-			data-neutral={style.neutral} data-brand={style.brand} data-accent={style.accent}
-			data-border={style.border} data-theme={style.theme}
-			data-solid={style.solid} data-solid-style={style.solidStyle}
-			data-surface={style.surface} data-transition={style.transition}
-			data-scaling={style.scaling}
-			className={classNames(
-				primary.variable, code.variable,
-				secondary ? secondary.variable : '',
-				tertiary ? tertiary.variable : ''
-			)}>
-			<head>
-				<script
-					type="application/ld+json"
-					dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
-				/>
-			</head>
-			<Flex
-				as="body"
-				fillWidth fillHeight margin="0" padding="0">
-				<Background
-					style={{zIndex: '-1'}}
-					position="fixed"
-					mask="cursor"
-					dots={{
-						display: true,
-						opacity: 0.4,
-						size: '20'
-					}}
-					gradient={{
-						display: true,
-						opacity: 0.4,
-					}}/>
-				<Flex
-					flex={1} direction="column">
-					{children}
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      as="html"
+      lang="en"
+      fillHeight
+      background="page"
+      data-neutral={style.neutral}
+      data-brand={style.brand}
+      data-accent={style.accent}
+      data-border={style.border}
+      data-theme={style.theme}
+      data-solid={style.solid}
+      data-solid-style={style.solidStyle}
+      data-surface={style.surface}
+      data-transition={style.transition}
+      data-scaling={style.scaling}
+      className={classNames(
+        primary.variable,
+        code.variable,
+        secondary ? secondary.variable : "",
+        tertiary ? tertiary.variable : "",
+      )}
+    >
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+        />
+      </head>
+      <Flex as="body" fillWidth fillHeight margin="0" padding="0">
+        <Background
+          style={{ zIndex: "-1" }}
+          position="fixed"
+          mask="cursor"
+          dots={{
+            display: true,
+            opacity: 0.4,
+            size: "40",
+          }}
+          gradient={{
+            display: true,
+            opacity: 0.4,
+          }}
+        />
+        <Flex flex={1} direction="column">
+          {children}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,142 +1,186 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 
-import { Heading, Text, Flex, Button, Grid, Icon, InlineCode, Logo, LetterFx, Arrow } from '@/once-ui/components';
-import Link from 'next/link';
+import {
+  Heading,
+  Text,
+  Flex,
+  Button,
+  Grid,
+  Icon,
+  InlineCode,
+  Logo,
+  LetterFx,
+  Arrow,
+  Calendar,
+  CalendarInput,
+} from "@/once-ui/components";
+import Link from "next/link";
+import { Select } from "@/once-ui/components/Select";
 
 export default function Home() {
-	const links = [
-		{
-			href: "https://once-ui.com/docs/theming",
-			title: "Themes",
-			description: "Style your app in minutes.",
-		},
-		{
-			href: "https://once-ui.com/docs/flexComponent",
-			title: "Layout",
-			description: "Build responsive layouts.",
-		},
-		{
-			href: "https://once-ui.com/docs/typography",
-			title: "Typography",
-			description: "Scale text automatically.",
-		},
-	];
+  const links = [
+    {
+      href: "https://once-ui.com/docs/theming",
+      title: "Themes",
+      description: "Style your app in minutes.",
+    },
+    {
+      href: "https://once-ui.com/docs/flexComponent",
+      title: "Layout",
+      description: "Build responsive layouts.",
+    },
+    {
+      href: "https://once-ui.com/docs/typography",
+      title: "Typography",
+      description: "Scale text automatically.",
+    },
+  ];
 
-	return (
-		<Flex
-			fillWidth paddingTop="l" paddingX="l"
-			direction="column" alignItems="center" flex={1}>
-			<Flex
-				position="relative"
-				as="section" overflow="hidden"
-				fillWidth minHeight="0" maxWidth={68}
-				direction="column" alignItems="center" flex={1}>
-				<Flex
-					as="main"
-					direction="column" justifyContent="center"
-					fillWidth fillHeight padding="l" gap="l">
-					<Flex
-						mobileDirection="column"
-						fillWidth gap="24">
-						<Flex
-							position="relative"
-							flex={2} paddingTop="56" paddingX="xl">
-							<Logo size="xl" icon={false} style={{zIndex: '1'}} href="https://once-ui.com"/>
-						</Flex>
-						<Flex
-							position="relative"
-							flex={4} gap="24" marginBottom="104"
-							direction="column">
-							<InlineCode
-								className="shadow-m"
-								style={{
-									width: 'fit-content',
-									padding: 'var(--static-space-8) var(--static-space-16)',
-									backdropFilter: 'blur(var(--static-space-1))'}}>
-								Start by editing <span className="brand-on-background-medium">app/page.tsx</span>
-							</InlineCode>
-							<Heading
-								wrap="balance"
-								variant="display-strong-s">
-								<span className="font-code">
-									<LetterFx
-										trigger="instant">
-										Helping designers code and developers design
-									</LetterFx>
-								</span>
-							</Heading>
-							<Button
-								id="readDocs"
-								href="https://once-ui.com/docs"
-								variant="secondary">
-								<Flex alignItems="center">
-									Read docs
-									<Arrow trigger="#readDocs"/>
-								</Flex>
-							</Button>
-						</Flex>
-					</Flex>
-					<Grid
-						radius="l"
-						border="neutral-medium"
-						borderStyle="solid-1"
-						columns="repeat(3, 1fr)"
-						tabletColumns="1col"
-						mobileColumns="1col"
-						fillWidth>
-						{links.map((link) => (
-							<Link
-								target="_blank"
-								style={{ padding: 'var(--responsive-space-l)' }}
-								key={link.href}
-								href={link.href}>
-								<Flex
-									fillWidth paddingY="8" gap="8"
-									direction="column">
-									<Flex
-										fillWidth gap="12"
-										alignItems="center">
-										<Text
-											variant="body-strong-m" onBackground="neutral-strong">
-											{link.title}
-										</Text>
-										<Icon size="s" name="arrowUpRight" />
-									</Flex>
-									<Text
-										variant="body-default-s" onBackground="neutral-weak">
-										{link.description}
-									</Text>
-								</Flex>
-							</Link>
-						))}
-					</Grid>
-				</Flex>
-			</Flex>
-			<Flex
-				as="footer"
-				position="relative"
-				fillWidth paddingX="l" paddingY="m"
-				justifyContent="space-between">
-				<Text
-					variant="body-default-s" onBackground="neutral-weak">
-					© 2024 Once UI, <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">MIT License</Link>
-				</Text>
-				<Flex
-					gap="12">
-					<Button
-						href="https://github.com/once-ui-system/nextjs-starter"
-						prefixIcon="github" size="s" variant="tertiary">
-						GitHub
-					</Button>
-					<Button
-						href="https://discord.com/invite/5EyAQ4eNdS"
-						prefixIcon="discord" size="s" variant="tertiary">
-						Discord
-					</Button>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      fillWidth
+      paddingTop="l"
+      paddingX="l"
+      direction="column"
+      alignItems="center"
+      flex={1}
+    >
+      <Flex
+        position="relative"
+        as="section"
+        overflow="hidden"
+        fillWidth
+        minHeight="0"
+        maxWidth={68}
+        direction="column"
+        alignItems="center"
+        flex={1}
+      >
+        <Flex
+          as="main"
+          direction="column"
+          justifyContent="center"
+          fillWidth
+          fillHeight
+          padding="l"
+          gap="l"
+        >
+          <Flex mobileDirection="column" fillWidth gap="24">
+            <Flex position="relative" flex={2} paddingTop="56" paddingX="xl">
+              <Logo
+                size="xl"
+                icon={false}
+                style={{ zIndex: "1" }}
+                href="https://once-ui.com"
+              />
+            </Flex>
+            <Flex
+              position="relative"
+              flex={4}
+              gap="24"
+              marginBottom="104"
+              direction="column"
+            >
+              <InlineCode
+                className="shadow-m"
+                style={{
+                  width: "fit-content",
+                  padding: "var(--static-space-8) var(--static-space-16)",
+                  backdropFilter: "blur(var(--static-space-1))",
+                }}
+              >
+                Start by editing{" "}
+                <span className="brand-on-background-medium">app/page.tsx</span>
+              </InlineCode>
+              <Heading wrap="balance" variant="display-strong-s">
+                <span className="font-code">
+                  <LetterFx trigger="instant">
+                    Helping designers code and developers design
+                  </LetterFx>
+                </span>
+              </Heading>
+              <Button
+                id="readDocs"
+                href="https://once-ui.com/docs"
+                variant="secondary"
+              >
+                <Flex alignItems="center">
+                  Read docs
+                  <Arrow trigger="#readDocs" />
+                </Flex>
+              </Button>
+            </Flex>
+          </Flex>
+          <Grid
+            radius="l"
+            border="neutral-medium"
+            borderStyle="solid-1"
+            columns="repeat(3, 1fr)"
+            tabletColumns="1col"
+            mobileColumns="1col"
+            fillWidth
+          >
+            {links.map((link) => (
+              <Link
+                target="_blank"
+                style={{ padding: "var(--responsive-space-l)" }}
+                key={link.href}
+                href={link.href}
+              >
+                <Flex fillWidth paddingY="8" gap="8" direction="column">
+                  <Flex fillWidth gap="12" alignItems="center">
+                    <Text variant="body-strong-m" onBackground="neutral-strong">
+                      {link.title}
+                    </Text>
+                    <Icon size="s" name="arrowUpRight" />
+                  </Flex>
+                  <Text variant="body-default-s" onBackground="neutral-weak">
+                    {link.description}
+                  </Text>
+                </Flex>
+              </Link>
+            ))}
+          </Grid>
+          Work in progress
+          <Calendar id="calendar" size="l" />
+          <CalendarInput id="calendarInput" label="Pick a date" />
+        </Flex>
+      </Flex>
+      <Flex
+        as="footer"
+        position="relative"
+        fillWidth
+        paddingX="l"
+        paddingY="m"
+        justifyContent="space-between"
+      >
+        <Text variant="body-default-s" onBackground="neutral-weak">
+          © 2024 Once UI,{" "}
+          <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">
+            MIT License
+          </Link>
+        </Text>
+        <Flex gap="12">
+          <Button
+            href="https://github.com/once-ui-system/nextjs-starter"
+            prefixIcon="github"
+            size="s"
+            variant="tertiary"
+          >
+            GitHub
+          </Button>
+          <Button
+            href="https://discord.com/invite/5EyAQ4eNdS"
+            prefixIcon="discord"
+            size="s"
+            variant="tertiary"
+          >
+            Discord
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
