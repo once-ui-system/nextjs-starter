010de4c626c2ee65e3b3f9de185598dcf8ad208d
Lorant <lorant@once-ui.com>
2024-12-12 21:48:29 +0100

    update page to test + demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Heading,
@@ -15,11 +15,20 @@ import {
   Arrow,
   Calendar,
   CalendarInput,
+  Dropdown,
 } from "@/once-ui/components";
 import Link from "next/link";
 import { Select } from "@/once-ui/components/Select";
+import { Option } from "@/once-ui/components/Option";
 
 export default function Home() {
+  const [selectedValue, setSelectedValue] = useState("Zebra");
+
+  const handleSelect = (value: string) => {
+    console.log("Selected option:", value);
+    setSelectedValue(value);
+  };
+
   const links = [
     {
       href: "https://once-ui.com/docs/theming",
@@ -105,11 +114,9 @@ export default function Home() {
                 id="readDocs"
                 href="https://once-ui.com/docs"
                 variant="secondary"
+                arrowIcon
               >
-                <Flex alignItems="center">
                   Read docs
-                  <Arrow trigger="#readDocs" />
-                </Flex>
               </Button>
             </Flex>
           </Flex>
@@ -144,8 +151,206 @@ export default function Home() {
             ))}
           </Grid>
           Work in progress
+          <Flex fillWidth padding="48" radius="xl" background="surface" data-theme="dark" direction="column" gap="48">
+            <Flex fillWidth wrap gap="12">
+              <Button data-solid="inverse" data-brand="blue">Button</Button>
+              <Button data-solid="inverse" data-brand="indigo">Button</Button>
+              <Button data-solid="inverse" data-brand="violet">Button</Button>
+              <Button data-solid="inverse" data-brand="magenta">Button</Button>
+              <Button data-solid="inverse" data-brand="pink">Button</Button>
+              <Button data-solid="inverse" data-brand="red">Button</Button>
+              <Button data-solid="inverse" data-brand="orange">Button</Button>
+              <Button data-solid="inverse" data-brand="yellow">Button</Button>
+              <Button data-solid="inverse" data-brand="moss">Button</Button>
+              <Button data-solid="inverse" data-brand="green">Button</Button>
+              <Button data-solid="inverse" data-brand="emerald">Button</Button>
+              <Button data-solid="inverse" data-brand="aqua">Button</Button>
+              <Button data-solid="inverse" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button data-solid="color" data-brand="blue">Button</Button>
+              <Button data-solid="color" data-brand="indigo">Button</Button>
+              <Button data-solid="color" data-brand="violet">Button</Button>
+              <Button data-solid="color" data-brand="magenta">Button</Button>
+              <Button data-solid="color" data-brand="pink">Button</Button>
+              <Button data-solid="color" data-brand="red">Button</Button>
+              <Button data-solid="color" data-brand="orange">Button</Button>
+              <Button data-solid="color" data-brand="yellow">Button</Button>
+              <Button data-solid="color" data-brand="moss">Button</Button>
+              <Button data-solid="color" data-brand="green">Button</Button>
+              <Button data-solid="color" data-brand="emerald">Button</Button>
+              <Button data-solid="color" data-brand="aqua">Button</Button>
+              <Button data-solid="color" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="blue">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="indigo">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="violet">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="magenta">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="pink">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="red">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="orange">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="yellow">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="moss">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="green">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="emerald">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="aqua">Button</Button>
+              <Button data-theme="dark" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="blue">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="indigo">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="violet">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="magenta">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="pink">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="red">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="orange">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="yellow">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="moss">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="green">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="emerald">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="aqua">Button</Button>
+              <Button data-theme="dark" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button size="l" data-solid="contrast" data-border="rounded" data-solid-style="plastic" data-brand="blue">Button</Button>
+              <Button size="m" data-solid="contrast" data-border="rounded" data-solid-style="plastic" data-brand="indigo">Button</Button>
+              <Button size="s" data-solid="contrast" data-border="rounded" data-solid-style="plastic" data-brand="violet">Button</Button>
+            </Flex>
+          </Flex>
+
+          <Flex fillWidth padding="48" topRadius="xl" bottomRightRadius="xl" background="page" data-theme="light" direction="column" gap="48">
+            <Flex fillWidth wrap gap="12">
+              <Button data-solid="inverse" data-brand="blue">Button</Button>
+              <Button data-solid="inverse" data-brand="indigo">Button</Button>
+              <Button data-solid="inverse" data-brand="violet">Button</Button>
+              <Button data-solid="inverse" data-brand="magenta">Button</Button>
+              <Button data-solid="inverse" data-brand="pink">Button</Button>
+              <Button data-solid="inverse" data-brand="red">Button</Button>
+              <Button data-solid="inverse" data-brand="orange">Button</Button>
+              <Button data-solid="inverse" data-brand="yellow">Button</Button>
+              <Button data-solid="inverse" data-brand="moss">Button</Button>
+              <Button data-solid="inverse" data-brand="green">Button</Button>
+              <Button data-solid="inverse" data-brand="emerald">Button</Button>
+              <Button data-solid="inverse" data-brand="aqua">Button</Button>
+              <Button data-solid="inverse" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button data-solid="color" data-brand="blue">Button</Button>
+              <Button data-solid="color" data-brand="indigo">Button</Button>
+              <Button data-solid="color" data-brand="violet">Button</Button>
+              <Button data-solid="color" data-brand="magenta">Button</Button>
+              <Button data-solid="color" data-brand="pink">Button</Button>
+              <Button data-solid="color" data-brand="red">Button</Button>
+              <Button data-solid="color" data-brand="orange">Button</Button>
+              <Button data-solid="color" data-brand="yellow">Button</Button>
+              <Button data-solid="color" data-brand="moss">Button</Button>
+              <Button data-solid="color" data-brand="green">Button</Button>
+              <Button data-solid="color" data-brand="emerald">Button</Button>
+              <Button data-solid="color" data-brand="aqua">Button</Button>
+              <Button data-solid="color" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="blue">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="indigo">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="violet">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="magenta">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="pink">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="red">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="orange">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="yellow">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="moss">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="green">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="emerald">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="aqua">Button</Button>
+              <Button data-theme="light" data-solid="inverse" data-border="conservative" data-solid-style="plastic" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="blue">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="indigo">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="violet">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="magenta">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="pink">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="red">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="orange">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="yellow">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="moss">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="green">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="emerald">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="aqua">Button</Button>
+              <Button data-theme="light" data-solid="color" data-border="rounded" data-solid-style="plastic" data-brand="cyan">Button</Button>
+            </Flex>
+            <Flex fillWidth wrap gap="12">
+              <Button size="l" data-solid="contrast" data-border="rounded" data-solid-style="plastic" data-brand="blue">Button</Button>
+              <Button size="m" data-solid="contrast" data-border="rounded" data-solid-style="plastic" data-brand="indigo">Button</Button>
+              <Button size="s" data-solid="contrast" data-border="rounded" data-solid-style="plastic" data-brand="violet">Button</Button>
+            </Flex>
+          </Flex>
+
+          <Flex fillWidth padding="48" radius="xl" background="surface" data-theme="dark" direction="column" gap="48">
+            <Flex fillWidth>
+              <Button variant="secondary" radius="left" data-solid="inverse" data-brand="blue">Button</Button>
+              <Button variant="secondary" radius="none" data-solid="inverse" data-brand="indigo">Button</Button>
+              <Button variant="secondary" radius="right" data-solid="inverse" data-brand="violet">Button</Button>
+            </Flex>
+            <Flex fillWidth negativeGap="1">
+              <Button variant="secondary" radius="left" data-solid="inverse" data-brand="blue">Button</Button>
+              <Button fillWidth prefixIcon="plus" suffixIcon="chevronDown" justifyContent="space-between" variant="secondary" radius="none" data-solid="inverse" data-brand="indigo">Button</Button>
+              <Button variant="secondary" radius="right" data-solid="inverse" data-brand="violet">Button</Button>
+            </Flex>
+          </Flex>
+          
+          <Select
+            id="select"
+            label="Select an option"
+            value={selectedValue}
+            options={
+              <>
+                <Option onClick={() => handleSelect("Zebra")} selected={selectedValue === "Zebra"} label="Zebra" value="Zebra" description="Option 1 description" />
+                <Option onClick={() => handleSelect("Lion")} selected={selectedValue === "Lion"} label="Lion" value="Lion" description="Option 2 description" />
+                <Option onClick={() => handleSelect("Mammal")} selected={selectedValue === "Mammal"} label="Mammal" value="Mammal" description="Option 3 description" />
+                <Option onClick={() => handleSelect("Coca Cola")} selected={selectedValue === "Coca Cola"} label="Coca Cola" value="Coca Cola" description="Option 4 description" />
+                <Option onClick={() => handleSelect("Chips")} selected={selectedValue === "Chips"} label="Chips" value="Chips" description="Option 5 description" />
+                <Option onClick={() => handleSelect("Computer")} selected={selectedValue === "Computer"} label="Computer" value="Computer" description="Option 6 description" />
+                <Option onClick={() => handleSelect("Keyboard")} selected={selectedValue === "Keyboard"} label="Keyboard" value="Keyboard" description="Option 7 description" />
+                <Option onClick={() => handleSelect("Movie")} selected={selectedValue === "Movie"} label="Movie" value="Movie" description="Option 8 description" />
+                <Option onClick={() => handleSelect("Wreck it Ralph")} selected={selectedValue === "Wreck it Ralph"} label="Wreck it Ralph" value="Wreck it Ralph" description="Option 9 description" />
+                <Option onClick={() => handleSelect("Binding")} selected={selectedValue === "Binding"} label="Binding" value="Binding" description="Option 10 description" />  
+              </>
+            }
+          />
           <Calendar id="calendar" size="l" />
           <CalendarInput id="calendarInput" label="Pick a date" />
+          <Flex fillWidth height={24} overflowY="scroll">
+            asd
+          </Flex>
+          <Button
+            id="github"
+            fillWidth
+            href="https://github.com/once-ui-system/nextjs-starter"
+            prefixIcon="github"
+            size="l"
+            justifyContent="space-between"
+            variant="tertiary"
+            arrowIcon
+          >
+            GitHub
+          </Button>
+          <Button
+            fillWidth
+            size="m"
+            variant="tertiary"
+          >
+            GitHub
+          </Button>
+          <Button
+            fillWidth
+            size="s"
+            variant="tertiary"
+          >
+            GitHub
+          </Button>
+
         </Flex>
       </Flex>
       <Flex
@@ -157,7 +362,7 @@ export default function Home() {
         justifyContent="space-between"
       >
         <Text variant="body-default-s" onBackground="neutral-weak">
-          © 2024 Once UI,{" "}
+          2024 Once UI,{" "}
           <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">
             MIT License
           </Link>
