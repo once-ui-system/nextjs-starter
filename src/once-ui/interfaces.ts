79c82a5a23024f1df837b0cd0b82afc095dd32fc
Lorant <lorant@once-ui.com>
2024-12-13 15:07:14 +0100

    add transition support to Flex

--- src/once-ui/interfaces.ts
+++ src/once-ui/interfaces.ts
@@ -121,6 +121,7 @@ export interface DisplayProps extends HTMLAttributes<HTMLDivElement> {
   overflow?: CSSProperties["overflow"];
   overflowX?: CSSProperties["overflowX"];
   overflowY?: CSSProperties["overflowY"];
+  transition?: "micro-short" | "micro-medium" | "micro-long" | "macro-short" | "macro-medium" | "macro-long";
   opacity?: 0 | 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100;
   zIndex?: -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 }
