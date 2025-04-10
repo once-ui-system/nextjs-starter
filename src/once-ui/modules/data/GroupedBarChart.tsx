af798a4e4b55f2e372019c9c644530e1db1c0157
Adhitya Nadooli <127269031+lightyfr@users.noreply.github.com>
2025-04-10 14:52:16 +0000

    fix GroupedBarChart Colors and Styles

--- src/once-ui/modules/data/GroupedBarChart.tsx
+++ src/once-ui/modules/data/GroupedBarChart.tsx
@@ -7,6 +7,7 @@ import {
   ResponsiveContainer,
   CartesianGrid,
   Tooltip,
+  Legend,
 } from "recharts";
 
 import moment from "moment";
@@ -27,229 +28,222 @@ interface MultiBarDataPoint {
 
 interface GroupedBarChartProps extends React.ComponentProps<typeof Flex> {
   data: MultiBarDataPoint[];
-  xAxisKey?: string;
-  yAxisKeys?: string[]; // Keys for multiple bars
-  barLabels?: string[]; // Labels for each bar series
   /**
-   * Size of the bar graph.
-   * @default "m"
+   * Which axis labels to show
+   * @default "both"
    */
+  labels?: "x" | "y" | "both" | "none";
+
+  xAxisKey?: string;
+  yAxisKeys?: string[];
+  barLabels?: string[];
   barWidth?: SpacingToken | "fill" | number | string;
-  blur?: boolean;
+  isTimeSeries?: boolean;
+  timeFormat?: string;
+  legend?: boolean;
   title?: string;
   description?: string;
   tooltipTitle?: string;
-  /**
-   * When true, hides labels on the X axis
-   * @default false
-   */
-  hideXAxisLabels?: boolean;
-  /**
-   * When true, hides labels on the Y axis
-   * @default false
-   */
-  hideYAxisLabels?: boolean;
-  /**
-   * When true, hides all axis labels
-   * @default false
-   */
-  hideLabels?: boolean;
-  /**
-   * When true, hides the X axis title
-   * @default false
-   */
-  hideXAxisTitle?: boolean;
-  /**
-   * When true, hides the Y axis title
-   * @default false
-   */
-  hideYAxisTitle?: boolean;
-  /**
-   * When true, hides all axis titles
-   * @default false
-   */
-  hideAxisTitles?: boolean;
-  /**
-   * Title for X-axis
-   */
-  xAxisTitle?: string;
-  /**
-   * Title for Y-axis
-   */
-  yAxisTitle?: string;
-  /**
-   * When true, hides the Y axis  title
-   * @default false
-   */
-  timeFormat?: string;
-
-   isTimeSeries?: boolean;
 }
 
-const CustomTooltip = ({ active, payload, tooltipTitle, barLabels, isTimeSeries, timeFormat }: any) => {
-  if (active && payload && payload.length) {
-    const data = payload[0].payload;
-    return (
-      <Flex minWidth={8} background="surface" border="neutral-alpha-medium" direction="column">
-        <Flex
-          borderBottom="neutral-alpha-medium"
-          fillWidth
-          horizontal="center"
-          padding="8"
-        >
-            <Text
-            variant="label-strong-s"
-            style={{ fontWeight: "600" }}
-            onBackground="neutral-strong"
-            >
-            {isTimeSeries ? moment(data.endDate || data.name).format(timeFormat) : (data.endDate?.toLocaleString() || data.name)}
-            </Text>
+const CustomLegend = ({ payload, labels, colors }: any) => {
+  if (!payload || !payload.length) return null;
+  return (
+    <Flex
+    horizontal="start" 
+    vertical="center" 
+    position="absolute"
+    gap="16"
+    left={(labels === "y" || labels === "both") ? "80" : "12"}
+    top="12"
+    >
+      {payload.map((entry: any, index: number) => {
+        const color = colors[index];
+        return (
+        <Flex key={index} vertical="center" gap="8">
+          <Flex
+            style={{
+              backgroundClip: "padding-box",
+              border: `1px solid ${color}`,
+              background: `linear-gradient(to bottom, ${color} 0%,
+               transparent 100%)`,
+            }}
+            minWidth="16"
+            minHeight="16"
+            radius="s"
+          />
+          <Text variant="label-default-s">{entry.value}</Text>
         </Flex>
-        <Flex padding="8" gap="4" direction="column">
-          {payload.map((entry: any, index: number) => (
-            <Text
-              variant="label-default-s"
-              key={`value-${index}`}
-              onBackground="neutral-strong"
-              style={{ color: entry.color }}
-            >
-              {`${isTimeSeries 
-              ? (barLabels?.[index] || moment(entry.dataKey).format(timeFormat))
-              : (barLabels?.[index] || entry.dataKey)
-              }: ${isTimeSeries ? moment(entry.value).format(timeFormat) : entry.value.toLocaleString()}`}
-            </Text>
-          ))}
-        </Flex>
+      );
+      })}
+    </Flex>
+  );
+};
+
+const CustomTooltip = ({
+  active,
+  payload,
+  isTimeSeries,
+  barLabels,
+  colors,
+  timeFormat = "YYYY-MM-DD",
+}: any) => {
+  if (!(active && payload && payload.length)) return null;
+  const data = payload[0].payload;
+  return (
+    <Column
+      minWidth={8}
+      gap="8"
+      background="surface"
+      radius="m"
+      border="neutral-alpha-medium"
+    >
+      <Flex fillWidth paddingTop="8" paddingX="12">
+        <Text variant="label-default-s" onBackground="neutral-strong">
+          {isTimeSeries
+            ? moment(data.name).format(timeFormat)
+            : (data.name || "—")}
+        </Text>
       </Flex>
-    );
-  }
-  return null;
+      <Column
+        fillWidth
+        horizontal="space-between"
+        paddingBottom="8"
+        paddingX="12"
+        gap="4"
+      >
+        {payload.map((entry: any, index: number) => {
+          const color = colors[index];
+          return (
+            <Flex key={index} horizontal="space-between" fillWidth gap="16">
+              <Flex vertical="center" gap="8">
+                <Flex
+                  style={{
+                    backgroundClip: "padding-box",
+                    border: `1px solid ${color}`,
+                    background: `linear-gradient(to bottom, ${color} 0%, transparent 100%)`
+                  }}
+                  minWidth="12"
+                  minHeight="12"
+                  radius="xs"
+                />
+                <Text onBackground="neutral-weak" variant="label-default-s">
+                  {barLabels?.[index] || entry.dataKey}
+                </Text>
+              </Flex>
+              <Text onBackground="neutral-strong" variant="label-default-s">
+                {typeof entry.value === "number"
+                  ? entry.value.toLocaleString()
+                  : entry.value}
+              </Text>
+            </Flex>
+          );
+        })}
+      </Column>
+    </Column>
+  );
 };
 
 const GroupedBarChart: React.FC<GroupedBarChartProps> = ({
   data,
+  labels = "both",
   xAxisKey = "name",
   yAxisKeys = ["value1", "value2", "value3"],
-  barWidth = "fill",
-  blur = false,
-  border,
-  title,
-  description,
-  tooltipTitle,
   barLabels,
-  background,
-  hideXAxisLabels = false,
-  xAxisTitle,
-  hideXAxisTitle = false,
-  hideYAxisLabels = false,
-  yAxisTitle,
-  hideYAxisTitle = false,
-  hideAxisTitles = false,
-  hideLabels = false,
+  barWidth = "fill",
   isTimeSeries = false,
   timeFormat,
+  title,
+  description,
+  legend = false,
+  tooltipTitle,
   ...flexProps
 }) => {
-
-
-  // Using the same colors from LineGraph
-  const barColors = ["var(--data-solid-100)", "var(--data-solid-500)", "var(--data-solid-300)"]; // green, red, purple
+  const barColors = [
+    "var(--data-blue)",
+    "var(--data-green)",
+    "var(--data-orange)",
+  ];
 
   return (
     <Flex
       fill
       radius="l"
-      border={border}
       align="center"
       direction="column"
       vertical="center"
       data-viz="categorical"
-      background={background}
       {...flexProps}
     >
-
-{title && (
-  <Column fillWidth borderBottom={border} horizontal="start" paddingX="20"
-  paddingY="12"
-  gap="4"
->
-      {title && (
-                <Text variant="heading-strong-s">
-                  {title}
-                </Text>
-              )}
-              {description && (
-                <Text variant="label-default-s" onBackground="neutral-weak">
-                  {description}
-                </Text>
-              )}
-              </Column>
-            )}
+      {(title || description) && (
+        <Column
+          fillWidth
+          borderBottom="neutral-medium"
+          horizontal="start"
+          paddingX="20"
+          paddingY="12"
+          gap="4"
+        >
+          {title && <Text variant="heading-strong-s">{title}</Text>}
+          {description && (
+            <Text variant="label-default-s" onBackground="neutral-weak">
+              {description}
+            </Text>
+          )}
+        </Column>
+      )}
       <Flex fill>
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
             data={data}
             margin={{ left: 0, bottom: 0, top: 0, right: 0 }}
             barGap={4}
-            >
+          >
             <CartesianGrid
-              horizontal={true}
+              horizontal
               vertical={false}
               stroke="var(--neutral-alpha-weak)"
             />
-                      <XAxis
-                        dataKey={xAxisKey}
-                        axisLine={false}
-                        tickLine={false}
-                        tick={hideXAxisLabels || hideLabels ? false : {
-                          fill: "var(--neutral-on-background-weak)",
-                          fontSize: 12,
-                        }}
-
-                        height={hideXAxisLabels || hideLabels ? 0 : 50}
-                        label={
-                          xAxisTitle && !hideXAxisTitle && !hideAxisTitles
-                            ? { value: xAxisTitle, fontWeight: "500", position: 'bottom', offset: -23, fill: "var(--neutral-on-background-medium)" }
-                            : undefined
-                        }
-                      />
-                      <YAxis
-                        allowDataOverflow
-                        axisLine={{
-                          stroke: "var(--neutral-alpha-medium)",
-                        }}
-                        tickLine={false}
-                        padding={{ top: 40 }}
-                        tick={{
-                          fill: "var(--neutral-on-background-weak)",
-                          fontSize: 12,
-                         }}
-                        width={yAxisTitle ? 54 : 0}
-                        label={
-                          yAxisTitle && !hideYAxisTitle && !hideAxisTitles
-                            ? { 
-                                value: yAxisTitle,
-                                position: 'insideTop',
-                                offset: 10,
-                                fontSize: 12,
-                                fill: "var(--neutral-on-background-medium)" 
-                              }
-                            : undefined
-                        }
-                      />
+            {legend && (
+              <Legend
+                content={<CustomLegend labels={labels} colors={barColors} />}
+                wrapperStyle={{ position: "absolute", top: 0, right: 0 }}
+              />
+            )}
+            {(labels === "x" || labels === "both") && (
+              <XAxis
+                dataKey={xAxisKey}
+                axisLine={false}
+                tickLine={false}
+                tick={{
+                  fill: "var(--neutral-on-background-weak)",
+                  fontSize: 12,
+                }}
+              />
+            )}
+            {(labels === "y" || labels === "both") && (
+              <YAxis
+                allowDataOverflow
+                axisLine={{ stroke: "var(--neutral-alpha-medium)" }}
+                tickLine={false}
+                padding={{ top: 40 }}
+                tick={{
+                  fill: "var(--neutral-on-background-weak)",
+                  fontSize: 12,
+                }}
+              />
+            )}
             <Tooltip
               content={
                 <CustomTooltip
-                  tooltipTitle={tooltipTitle}
-                  barLabels={barLabels}
+                  colors={barColors}
                   isTimeSeries={isTimeSeries}
+                  barLabels={barLabels}
                   timeFormat={timeFormat}
                 />
               }
               cursor={{ fill: "rgba(255,255,255,0.05)" }}
             />
-
-            {/* Define gradients for each bar color */}
             <defs>
               {barColors.map((color, index) => (
                 <linearGradient
@@ -265,8 +259,6 @@ const GroupedBarChart: React.FC<GroupedBarChartProps> = ({
                 </linearGradient>
               ))}
             </defs>
-
-            {/* Create a Bar component for each data key */}
             {yAxisKeys.map((key, index) => (
               <Bar
                 key={key}
@@ -278,18 +270,18 @@ const GroupedBarChart: React.FC<GroupedBarChartProps> = ({
                 radius={[6, 6, 0, 0]}
                 barSize={
                   barWidth === "fill"
-                  ? "100%"
-                  : barWidth === "xs"
-                  ? 12
-                  : barWidth === "s"
-                  ? 16
-                  : barWidth === "m"
-                  ? 24
-                  : barWidth === "l"
-                  ? 40
-                  : barWidth === "xl"
-                  ? 64
-                  : barWidth
+                    ? "100%"
+                    : barWidth === "xs"
+                    ? 12
+                    : barWidth === "s"
+                    ? 16
+                    : barWidth === "m"
+                    ? 24
+                    : barWidth === "l"
+                    ? 40
+                    : barWidth === "xl"
+                    ? 64
+                    : barWidth
                 }
                 isAnimationActive={false}
               />
