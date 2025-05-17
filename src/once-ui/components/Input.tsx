a73b90b5f4a6849996e10afaafbfd6b7602c1ed3
Justin <justin.eiletz@outlook.com>
2025-05-17 13:57:21 +0200

    Input fix, label gonna float as well, if placeholder is placed to prevent label to be in front of the placeholder...

--- src/once-ui/components/Input.tsx
+++ src/once-ui/components/Input.tsx
@@ -178,7 +178,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                 variant="label-default-m"
                 htmlFor={id}
                 className={classNames(styles.label, styles.inputLabel, {
-                  [styles.floating]: isFocused || isFilled,
+                  [styles.floating]: isFocused || isFilled || placeholder,
                 })}
               >
                 {label}
