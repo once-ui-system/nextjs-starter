fcc7352c83490da167e218f65e2fec5edc5bd17a
Lorant <lorant@once-ui.com>
2025-06-21 12:25:32 +0200

    feat: themed logo

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -22,7 +22,8 @@ export default function Home() {
           vertical="center"
           gap="16"
         >
-          <Logo icon="/trademarks/type-dark.svg" href="https://once-ui.com" size="xs" />
+          <Logo dark icon="/trademarks/wordmark-dark.svg" href="https://once-ui.com" size="xs" />
+          <Logo light icon="/trademarks/wordmark-light.svg" href="https://once-ui.com" size="xs" />
           <Line vert background="neutral-alpha-strong" />
           <Text marginX="4">
             <LetterFx trigger="instant">An ecosystem, not a UI kit</LetterFx>
