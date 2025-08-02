66486e37b0e08183ef309e9332d4aa0213e25e0f
Lorant <lorant@once-ui.com>
2025-08-02 16:13:55 +0200

    release: update to Once UI Core 1.4+

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,42 +1,44 @@
 "use client";
 
-import { BorderStyle, ChartMode, ChartVariant, DataThemeProvider, IconProvider, NeutralColor, ScalingSize, Schemes, SolidStyle, SolidType, SurfaceStyle, Theme, ThemeProvider, ToastProvider, TransitionStyle } from "@once-ui-system/core";
+import { BorderStyle, ChartMode, ChartVariant, DataThemeProvider, IconProvider, LayoutProvider, NeutralColor, ScalingSize, Schemes, SolidStyle, SolidType, SurfaceStyle, Theme, ThemeProvider, ToastProvider, TransitionStyle } from "@once-ui-system/core";
 import { style, dataStyle } from "../resources/once-ui.config";
 import { iconLibrary } from "../resources/icons";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      theme={style.theme as Theme}
-      brand={style.brand as Schemes}
-      accent={style.accent as Schemes}
-      neutral={style.neutral as NeutralColor}
-      solid={style.solid as SolidType}
-      solidStyle={style.solidStyle as SolidStyle}
-      border={style.border as BorderStyle}
-      surface={style.surface as SurfaceStyle}
-      transition={style.transition as TransitionStyle}
-      scaling={style.scaling as ScalingSize}
-    >
-      <DataThemeProvider
-        variant={dataStyle.variant as ChartVariant}
-        mode={dataStyle.mode as ChartMode}
-        height={dataStyle.height}
-        axis={{
-          stroke: dataStyle.axis.stroke
-        }}
-        tick={{
-          fill: dataStyle.tick.fill,
-          fontSize: dataStyle.tick.fontSize,
-          line: dataStyle.tick.line
-        }}
-        >
-        <ToastProvider>
-          <IconProvider icons={iconLibrary}>
-            {children}
-          </IconProvider>
-        </ToastProvider>
-      </DataThemeProvider>
-    </ThemeProvider>
+    <LayoutProvider>
+      <ThemeProvider
+        theme={style.theme as Theme}
+        brand={style.brand as Schemes}
+        accent={style.accent as Schemes}
+        neutral={style.neutral as NeutralColor}
+        solid={style.solid as SolidType}
+        solidStyle={style.solidStyle as SolidStyle}
+        border={style.border as BorderStyle}
+        surface={style.surface as SurfaceStyle}
+        transition={style.transition as TransitionStyle}
+        scaling={style.scaling as ScalingSize}
+      >
+        <DataThemeProvider
+          variant={dataStyle.variant as ChartVariant}
+          mode={dataStyle.mode as ChartMode}
+          height={dataStyle.height}
+          axis={{
+            stroke: dataStyle.axis.stroke
+          }}
+          tick={{
+            fill: dataStyle.tick.fill,
+            fontSize: dataStyle.tick.fontSize,
+            line: dataStyle.tick.line
+          }}
+          >
+          <ToastProvider>
+            <IconProvider icons={iconLibrary}>
+              {children}
+            </IconProvider>
+          </ToastProvider>
+        </DataThemeProvider>
+      </ThemeProvider>
+    </LayoutProvider>
   );
 }
